e352aa281591d3c6015dd3e2c063ec54c536c838
Hussain <hussain.gagan@gmail.com>
2021-03-05 10:15:30 +0530

    Added Admin module and added faculty data from admin side

--- app.js
+++ app.js
@@ -10,14 +10,30 @@ app.set('view engine', 'ejs');
 
 mongoose.connect("mongodb://localhost:27017/facMangDB", {useNewUrlParser : true , useUnifiedTopology: true });
 
+///////// ------------------------- Faculty Schema ---------------------- //////
 const facSchema = new mongoose.Schema({
   facId : Number,
   name : String,
   email : String,
-  password : String
+  password : String,
+  role : String,
+  areaOfInterest : String
 })
 const Faculty = mongoose.model("faculty",facSchema);
 
+///////// ------------------------- Admin Schema ---------------------- //////
+const adminSchema = new mongoose.Schema({
+  adminId : Number,
+  password : String
+})
+const Admin = mongoose.model("admin",adminSchema);
+const admin = new Admin({
+  adminId : 1523,
+  password: "qwerty"
+})
+// admin.save();
+
+///////// ------------------------- Faculty module ---------------------- //////
 app.get("/",function(res,res){
   res.render("facLogin");
 })
@@ -73,6 +89,63 @@ app.get("/freeSlot",function(res,res){
 })
 
 
+////---------------------------Admin module -----------------------------///////////////
+app.get("/admin",(req,res)=>{
+  res.render("adminLogin");
+})
+
+app.post("/admin",(req,res)=>{
+  var adminId = req.body.adminId;
+  var adminPass = req.body.adminPass;
+  Admin.findOne({adminId : adminId}, function(err, foundAdmin){
+    if(err){
+      console.log(err);
+    }else{
+      if(foundAdmin){
+        if(foundAdmin.password === adminPass){
+          res.render("admFaculty");
+        }
+      }
+    }
+  })
+})
+
+app.get("/admFaculty",function(req,res){
+  res.render("admFaculty");
+})
+
+app.post("/admFaculty",function(req,res){
+    var regFacId = req.body.facAdminId;
+    var regFacName = req.body.facultyAdminName;
+    var regFacEmail = req.body.facAdminEmail;
+    var regFacPass = req.body.facAdminPass;
+    var regFacAreaOfInterest = req.body.areaOfInterest;
+    var regFacRole = req.body.facRole;
+    const faculty = new Faculty({
+        facId : regFacId,
+        name : regFacName,
+        email : regFacEmail,
+        password : regFacPass,
+        role  : regFacRole,
+        areaOfInterest : regFacAreaOfInterest
+      })
+      faculty.save(function(err){
+        if(!err){
+          console.log("Succesfully added faculty to the database");
+          res.redirect("/admFaculty");
+        }
+      });
+})
+
+app.get("/admStudent",function(req,res){
+  res.render("admStudent");
+})
+app.get("/admTimeTable",function(res,res){
+  res.render("admTimeTable");
+})
+
+
+///------------------- starting port at 3000 ------------------//////////////////
 app.listen(3000,function(){
   console.log("Server started at port 3000");
 })
